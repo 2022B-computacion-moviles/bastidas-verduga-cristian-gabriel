c1e4da5572316fe46ae1087d669edfd1ea0b3cd3
Cristian Bastidas <gabrielbastidas5g@outlook.com>
2023-02-11 17:36:18 -0500

    CRUD: SQLite

--- CrudSQLite/app/src/main/java/com/example/crudsqlite/EstudianteView.kt
+++ CrudSQLite/app/src/main/java/com/example/crudsqlite/EstudianteView.kt
@@ -3,7 +3,7 @@ package com.example.crudsqlite
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Estudiantes : AppCompatActivity() {
+class EstudianteView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_estudiante)
--- CrudSQLite/app/src/main/java/com/example/crudsqlite/MateriaView.kt
+++ CrudSQLite/app/src/main/java/com/example/crudsqlite/MateriaView.kt
@@ -3,7 +3,7 @@ package com.example.crudsqlite
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Materias : AppCompatActivity() {
+class MateriaView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_materia)
